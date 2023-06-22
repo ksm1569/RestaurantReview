1c91042ae30a6fad439645c17309c5bc5080936f
suminkim <tnals1569@gmail.com>
2023-06-22 22:17:06 +0900

    맛집 api 통신테스트

---
+++ src/main/java/com/smsoft/review/api/RestaurantApi.java
@@ -0,0 +1,32 @@
+package com.smsoft.review.api;
+
+import com.smsoft.review.api.request.CreateAndEditRestaurantRequest;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class RestaurantApi {
+    @GetMapping("/restaurants")
+    public String getRestaurants(){
+        return "This is getRestaurants";
+    }
+    @GetMapping("/restaurant/{restaurantId}")
+    public String getRestaurant(@PathVariable Long restaurantId) {
+        return "This is getRestaurant, " + restaurantId;
+    }
+
+    @PostMapping("/restaurant")
+    public String createRestaurant(@RequestBody CreateAndEditRestaurantRequest request){
+        return "This is createRestaurant, name=" + request.getName() + "address=" + request.getAddress()
+                + ", menu[0].name= " + request.getMenus().get(0).getName() + ", menu[0].price = " + request.getMenus().get(0).getPrice();
+    }
+
+    @PutMapping("/restaurant/{restaurantId}")
+    public String editRestaurant(@PathVariable Long restaurantId, @RequestBody CreateAndEditRestaurantRequest request){
+        return "This is editRestaurant, " + restaurantId + "name=" + request.getName() + "address=" + request.getAddress();
+    }
+
+    @DeleteMapping("/restaurant/{restaurantId}")
+    public String deleteRestaurant(@PathVariable Long restaurantId){
+        return "This is deleteRestaurant, " + restaurantId;
+    }
+}
---
+++ src/main/java/com/smsoft/review/api/request/CreateAndEditRestaurantRequest.java
@@ -0,0 +1,16 @@
+package com.smsoft.review.api.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateAndEditRestaurantRequest {
+    private String name;
+    private String address;
+    private List<CreateAndEditRestaurantRequestMenu> menus;
+}
