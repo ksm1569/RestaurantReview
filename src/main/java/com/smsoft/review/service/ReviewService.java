71407c652e68647ffdaf790b7c511f2fe7e86c96
suminkim <tnals1564@naver.com>
2023-06-26 17:41:28 +0900

    라스트커밋

--- src/main/java/com/smsoft/review/service/ReviewService.java
+++ src/main/java/com/smsoft/review/service/ReviewService.java
@@ -35,6 +35,7 @@ public class ReviewService {
     public void deleteReview(Long reviewId){
         ReviewEntity review = reviewRepository.findById(reviewId).orElseThrow();
 
+
         reviewRepository.delete(review);
     }
 
